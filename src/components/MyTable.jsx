212f9b1401e6fc6ebeb1426c984f437cf31bb62f
1169969860@qq.com <1169969860@qq.com>
2020-06-27 12:07:28 +0800

    delete completed

--- src/components/MyTable.jsx
+++ src/components/MyTable.jsx
@@ -122,7 +122,7 @@ function MyTable(props) {
 
   // 删
   const deleteBook = key => {
-    axios.post(`/api/v1/book/delete/${key}`).then(() => {
+    axios.post(`/api/delete/${key}`).then(() => {
       const newData = [...bookList];
       const index = newData.findIndex(item => key === item.id);
       newData.splice(index, 1);
